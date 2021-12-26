5c2c751aafc62de5c0747ade3323c4bddd46467a
priyanshu parashar <priyanshuparashar223@gmail.com>
2021-12-26 14:02:27 +0530

    cart page, product page, page linking urls done

--- autoecom/base/urls.py
+++ autoecom/base/urls.py
@@ -5,8 +5,9 @@ urlpatterns = [
 
     path('',views.home, name="home"),
     path('product/',views.product, name="product"),
-    path('cart/',views.cart, name="cart"),
+    path('cart/', views.cart, name="cart"),
     path('userdash/',views.user_dash, name="user profile"),
+    path('productlist/', views.productlist, name="productlist"),
  
 
 
--- autoecom/base/views.py
+++ autoecom/base/views.py
@@ -3,21 +3,24 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    con=True;
+   
     loop=range(1,8)
     
-    return render(request, 'index.html', {'con':con, 'loop':loop})
+    return render(request, 'index.html', {'loop':loop})
 
 
 def product(request):
-    return render (request, 'product.html')
+    return render(request, 'product.html')
 
 
 def cart(request):
-    return render (request, 'cart.html')
+    return render(request, 'cart.html')
 
 
 
 def user_dash(request):
     return render (request, 'user_profile/dashboard.html')
 
+def productlist(request):
+    loop=range(1,8)
+    return render(request, 'productlist.html', {'loop':loop})
